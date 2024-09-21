2c8ea488dd6b3f467bd2694245fd454f1fed63a4
Ayoub SABI <ayoub.sabi@gmail.com>
2024-09-21 15:57:13 +0100

    Create product and category models and there migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function parent()
+    {
+        return $this->belongsTo(self::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(self::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
