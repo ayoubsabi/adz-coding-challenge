6e872408c9e91e8588780f189c470f646b591b76
Ayoub SABI <ayoub.sabi@gmail.com>
2024-09-21 16:21:57 +0100

    Implement ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use App\Services\Product\ProductService;
+use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
+
+class ProductController extends Controller
+{
+    public function __construct(
+        readonly private ProductService $productService
+    ) { }
+
+    /**
+     * Display a listing of the resource.
+     * 
+     * @param Request $request
+     * 
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        return new JsonResponse(
+            new ProductCollection(
+                $this->productService->getProducts($request->all())
+            ),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param Request $request
+     * 
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        return new JsonResponse(
+            new ProductResource(
+                $this->productService->createProduct($request->all())
+            ),
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param int $id
+     * 
+     * @return JsonResponse
+     */
+    public function show(int $id): JsonResponse
+    {
+        return new JsonResponse(
+            new ProductResource(
+                $this->productService->getProductById($id)
+            ),
+            Response::HTTP_CREATED
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param Request $request
+     * @param int $id
+     * 
+     * @return JsonResponse
+     */
+    public function update(Request $request, int $id): JsonResponse
+    {
+        return new JsonResponse(
+            new ProductResource(
+                $this->productService->updateProduct($id, $request->all())
+            ),
+            Response::HTTP_OK
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param int $id
+     * 
+     * @return JsonResponse
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        $this->productService->deleteProduct($id);
+
+        return new JsonResponse(status: Response::HTTP_NO_CONTENT);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show'])->where('id', '[0-9]+');
+Route::post('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+'); // I use post method because put and patch methods not support files upload
+Route::delete('/products/{id}', [ProductController::class, 'delete'])->where('id', '[0-9]+');
